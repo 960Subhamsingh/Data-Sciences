b5052ec59937c05960c5899ce4f18113eeb16bdb
Subham Kumar <158332737+960Subhamsingh@users.noreply.github.com>
2024-12-23 22:38:19 +0530

    Pandas Install

---
+++ pandas/01-Installation/Installl.py
@@ -0,0 +1,19 @@
+
+""""
+Pandas is a Python library used for working with data sets. 
+It has functions for analyzing, cleaning, exploring, and manipulating data.
+"""
+
+# pip install pandas 
+
+import pandas as pd
+
+import warnings
+
+warnings.warn("Setuptools is replacing")
+
+# show installed version
+print(pd.__version__)
+
+print(pd.show_versions())
+
